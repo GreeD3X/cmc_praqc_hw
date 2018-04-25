169b3586592b1d862916ab10e11b3e30cd062879
GreeD3X <griganic@gmail.com>
2018-04-25 13:57:33 +0300

    fixed template issue
    (cant split them on hpp and cpp)

--- grammars/include/filter.hpp
+++ grammars/include/filter.hpp
@@ -31,4 +31,45 @@ icurrent(begin), iend(end), pred(pred) {
 		++icurrent;
 }//cause while in filter.cpp it wouldnt compile
 
+template<class iterator_type, class T>
+const T FilterIterator<iterator_type,T>::operator*() const {
+	return *icurrent;
+}
+
+template<class iterator_type, class T>
+const T FilterIterator<iterator_type,T>::operator->() const {
+	return *icurrent; 
+}
+
+template<class iterator_type, class T>
+FilterIterator<iterator_type,T>& FilterIterator<iterator_type,T>::operator++() { 
+	advance(); 
+	return *this; 
+}
+
+template<class iterator_type, class T>
+bool FilterIterator<iterator_type,T>::operator==(const FilterIterator& other) const { 
+	return icurrent == other.icurrent; 
+}
+
+template<class iterator_type, class T>
+bool FilterIterator<iterator_type,T>::operator!=(const FilterIterator& other) const { 
+	return !(operator==(other)); 
+}
+
+template<class iterator_type, class T>
+const FilterIterator<iterator_type,T> FilterIterator<iterator_type,T>::end() const {
+	return {this->iend, this->iend, this->pred};
+}
+
+template<class iterator_type, class T>
+void FilterIterator<iterator_type,T>::advance(){
+    do
+    {
+        ++icurrent;
+    }
+    while((icurrent != iend) && (!pred(*icurrent)));
+} // advance
+
+
 #endif
